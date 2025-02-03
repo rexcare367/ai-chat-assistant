4ae1293e10704f43246db47e3d6fa0bc7e995ec4
rexcare <clever.fox.w@gmail.com>
2025-02-03 17:47:23 -0500

    feat/add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -12,16 +12,36 @@ const Home = () => {
   };
 
   return (
-    <main className={styles.main}>
-      <div className={styles.title}>
-        Explore sample apps built with Assistants API
-      </div>
-      <div className={styles.container}>
-        {Object.entries(categories).map(([name, url]) => (
-          <a key={name} className={styles.category} href={`/examples/${url}`}>
-            {name}
+    <main
+      className={styles.main}
+      style={{
+        backgroundImage:
+          'url("https://images.unsplash.com/photo-1677442136019-21780ecad995?auto=format&fit=crop&q=80")',
+        backgroundSize: "cover",
+      }}
+    >
+      <div
+        style={{
+          width: "80%",
+          height: "80%",
+          background: "rgba(255, 255, 255, 0.4)",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "1rem",
+        }}
+      >
+        <div className={styles.title}>AI Assistants Chatbot</div>
+        <div className={styles.container}>
+          <a
+            className={styles.category}
+            href={`/sign-in`}
+            style={{ width: "fit-content" }}
+          >
+            Get Started
           </a>
-        ))}
+        </div>
       </div>
     </main>
   );
