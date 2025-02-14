45ff8cae08273a41a26a878de49fd45650b834da
rexcare <clever.fox.w@gmail.com>
2025-02-14 12:14:29 -0500

    feat/update asssitant tools

--- app/api/assistants/route.ts
+++ app/api/assistants/route.ts
@@ -10,46 +10,46 @@ export async function POST(_request) {
     name: "Quickstart Assistant",
     model: "gpt-4o-mini",
     tools: [
-      { type: "code_interpreter" },
-      {
-        type: "function",
-        function: {
-          name: "get_weather",
-          description: "Determine weather in my location",
-          parameters: {
-            type: "object",
-            properties: {
-              location: {
-                type: "string",
-                description: "The city and state e.g. San Francisco, CA",
-              },
-              unit: {
-                type: "string",
-                enum: ["c", "f"],
-              },
-            },
-            required: ["location"],
-          },
-        },
-      },
-      {
-        type: "function",
-        function: {
-          name: "getCountryInformation",
-          description:
-            "Get detailed information of a country like position, size, population, flag and other symbols",
-          parameters: {
-            type: "object",
-            properties: {
-              country: {
-                type: "string",
-                description: "Country name, e.g. Sweden",
-              },
-            },
-            required: ["country"],
-          },
-        },
-      },
+      // { type: "code_interpreter" },
+      // {
+      //   type: "function",
+      //   function: {
+      //     name: "get_weather",
+      //     description: "Determine weather in my location",
+      //     parameters: {
+      //       type: "object",
+      //       properties: {
+      //         location: {
+      //           type: "string",
+      //           description: "The city and state e.g. San Francisco, CA",
+      //         },
+      //         unit: {
+      //           type: "string",
+      //           enum: ["c", "f"],
+      //         },
+      //       },
+      //       required: ["location"],
+      //     },
+      //   },
+      // },
+      // {
+      //   type: "function",
+      //   function: {
+      //     name: "getCountryInformation",
+      //     description:
+      //       "Get detailed information of a country like position, size, population, flag and other symbols",
+      //     parameters: {
+      //       type: "object",
+      //       properties: {
+      //         country: {
+      //           type: "string",
+      //           description: "Country name, e.g. Sweden",
+      //         },
+      //       },
+      //       required: ["country"],
+      //     },
+      //   },
+      // },
       {
         type: "function",
         function: {
